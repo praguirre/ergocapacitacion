d1476d294ff198af96a7ad8889426a7bf1568bc7
Pablo R Aguirre <praguirre@gmail.com>
2026-01-21 13:06:01 -0300

    Commit 1: add health check endpoint on /health/

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def health(_request):
+    """Endpoint simple para verificar que el servidor responde."""
+    return HttpResponse("OK")
